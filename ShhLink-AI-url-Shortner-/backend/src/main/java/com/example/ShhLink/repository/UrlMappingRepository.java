72ae477cc210dac8c1451f3c7bb984f63fea549d
WISEYNAB <109348369+WISEYNAB@users.noreply.github.com>
2025-09-04 01:17:52 +0530

    fixed repo structure

---
+++ ShhLink-AI-url-Shortner-/backend/src/main/java/com/example/ShhLink/repository/UrlMappingRepository.java
@@ -0,0 +1,14 @@
+package com.example.ShhLink.repository;
+
+import com.example.ShhLink.models.UrlMapping;
+import com.example.ShhLink.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UrlMappingRepository extends JpaRepository<UrlMapping, Long> {
+    UrlMapping findByShortUrl(String shortUrl);
+    List<UrlMapping> findByUser(User user);
+}
---
+++ ShhLink-AI-url-Shortner-/backend/src/test/java/com/example/ShhLink/ShhLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShhLink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShhLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
